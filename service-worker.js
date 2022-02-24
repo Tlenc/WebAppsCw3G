49379e2513206772090b9b04d41039ad47154b9b
Arek Herdzik <arekhbm1@gmail.com>
2022-02-24 15:44:37 +0000

    fixed service-worker

--- service-worker.js
+++ service-worker.js
@@ -7,3 +7,25 @@ var cacheFiles = [
 'images/icon-512.png',
 'styleSheet.css',
 ];
+
+self.addEventListener('install', (e) => {
+    console.log('[Service Worker] Install');
+    e.waitUntil(
+        caches.open(cacheName).then((cache) => {
+        console.log('[Service Worker] Caching all the files');
+        return cache.addAll(cacheFiles);
+        })
+    );
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith(
+        // check if the cache has the file
+        caches.match(e.request).then(function (r) {
+            console.log('[Service Worker] Fetching resource: '
+            + e.request.url);
+            // 'r' is the matching file if it exists in the cache
+            return r
+        })
+    );
+});
