7977a564222fa4d8e48a5ce3e06c890315c15421
Arek Herdzik <arekhbm1@gmail.com>
2022-02-24 15:23:32 +0000

    adding files for caching to service worker

---
+++ service-worker.js
@@ -0,0 +1,9 @@
+var cacheName = 'lessonStore-v1';
+var cacheFiles = [
+'lessonStore.html',
+'lessonStore.webmanifest',
+'README.md',
+'images/icon-192.png',
+'images/icon-512.png',
+'styleSheet.css',
+];
